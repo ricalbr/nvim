59584a8da9f34c5650cb79cb4279f0600485419f
ricalbr <albr.riccardo@gmail.com>
2022-11-01 15:02:36 +0100

    Add lua config file

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,10 @@
+-- lua
+
+-- basic settings
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.fileformat = 'unix'
+vim.opt.foldmethod = 'marker'
+vim.opt.foldlevel = 0
