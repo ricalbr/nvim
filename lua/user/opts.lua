158fd3b991badf9ac722657ebcb9b7a19bebc9b4
ricalbr <albr.riccardo@gmail.com>
2022-11-01 15:10:51 +0100

    Remove nvim default settings
    See `:h nvim-defaults`.

--- lua/user/opts.lua
+++ lua/user/opts.lua
@@ -1,25 +1,30 @@
--- vim.api.nvim_command('filetype plugin indent on')
--- vim.cmd('syntax on')
-
-vim.opt.autoindent = true
-vim.opt.backspace = 'indent,eol,start'
-vim.opt.hidden = true
-vim.g.noswapfile = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.hlsearch = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.cindent = true
 vim.opt.cinoptions = ':.5s,>1s,p0,t0,(0,g2'
 vim.opt.cinwords = 'if,else,while,do,for,switch,case,class,try'
-vim.opt.termguicolors = true
+
+vim.opt.path = '.,**'
+vim.opt.scrolljump = 5
+vim.opt.scrolloff = 999
+vim.opt.sidescrolloff = 5
+vim.g.noshowmode = true
+vim.opt.lazyredraw = true
+vim.opt.redrawtime = 100
+vim.opt.synmaxcol = 0
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.numberwidth = 5
+vim.opt.signcolumn = 'yes'
+
+vim.opt.undofile = true
+vim.g.noswapfile = true
 
 -- listchars options
 vim.opt.list = true
@@ -36,15 +41,12 @@ vim.opt.fillchars = {
   diff = '∙',       -- BULLET OPERATOR (U+2219, UTF-8: E2 88 99)
   eob = ' ',        -- NO-BREAK SPACE (U+00A0, UTF-8: C2 A0) to suppress ~ at EndOfBuffer
   fold = '·',       -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
-  vert = '┃',       -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83) 
-  diff = '∙',       -- BULLET OPERATOR (U+2219, UTF-8: E2 88 99)
-  fold = ·,         -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
-} 
+  vert = '┃',       -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
+}
 
 vim.opt.wrap = true
 vim.opt.breakindent = true
 vim.opt.linebreak = true
-vim.opt.nrformats: remove{'octal'}
 vim.opt.diffopt: append{'foldcolumn:0', 'vertical'}
 vim.g.splitvertical = true
 vim.g.splitright = true
@@ -53,35 +55,20 @@ vim.g.splitbelow = true
 
 -- folding options
 vim.opt.foldlevelstart=99
-vim.wo.foldmethod = 'indent' 
 -- qui è un buon punto per partire https://github.com/wincent/wincent/commit/6c98fcb169345131e37e856d6ea2d264e61f3820
--- vim.opt.foldtext = 'user.foldtext()'
--- vim.wo.foldmethod = 'expr'
--- vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+-- vim.opt.foldtext = 'user.foldtext'
 
--- delete comment character when joining commented lines
-vim.opt.formatoptions: append{'j'}
-
-vim.opt.wildmenu = true
 vim.opt.wildmode = 'longest:full,full'
 vim.opt.wildignore: append {'*.bak','*.swp','*.swo','*.a','*.o','*.so','*.pyc','*.class', '*.jpg','*.jpeg','*.gif','*.png','*.pdf', '*/.git*','*.tar','*.zip', '*.obj','*.exe','*.py[co]','.svn'}
 vim.opt.completeopt = 'longest,menuone'
 vim.opt.omnifunc = 'syntaxcomplete#Complete'
 
-vim.opt.path = '.,**'
-vim.opt.scrolljump = 5
-vim.opt.scrolloff = 999
-vim.opt.sidescrolloff = 5
-vim.g.noshowmode = true
-vim.opt.lazyredraw = true
-vim.opt.redrawtime = 10000
-vim.opt.synmaxcol = 0
-vim.opt.number = true
-
-vim.opt.relativenumber = true
-
-vim.opt.numberwidth = 5
-vim.opt.signcolumn = 'yes'
-vim.opt.undofile = true
-vim.opt.encoding = 'utf-8'
-
+vim.cmd[[
+augroup Operations
+  autocmd!
+  autocmd BufReadPost * call autocmdfunctions#LastPosition()
+  autocmd BufWritePre * call autocmdfunctions#RmvTrailingSpaces()
+augroup END
+]]
