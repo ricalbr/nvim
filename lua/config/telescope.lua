27072a3e5f074b3f4dc6f1c4016024d5e82a7d96
ricalbr <albr.riccardo@gmail.com>
2023-01-13 10:39:14 +0100

    Fix formatting

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -2,25 +2,25 @@
 
 local telescope = require("telescope")
 telescope.setup({
-    defaults = {
-        path_display = { "smart" },
-        mappings = {
-            i = {
-                ["?"] = require("telescope.actions.layout").toggle_preview, -- preview is disabled by default
-            },
-        },
-    },
+	defaults = {
+		path_display = { "smart" },
+		mappings = {
+			i = {
+				["?"] = require("telescope.actions.layout").toggle_preview, -- preview is disabled by default
+			},
+		},
+	},
 })
 
 -- keymaps
 vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
 vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
 vim.keymap.set("n", "<leader>/", function()
-    -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-        winblend = 10,
-        previewer = false,
-    }))
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
 end, { desc = "[/] Fuzzily search in current buffer]" })
 
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
@@ -28,6 +28,7 @@ vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
 
 pcall(require("telescope").load_extension, "fzf")
 pcall(require("telescope").load_extension, "frecency")
