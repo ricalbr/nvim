1acdaf76f6b6d0a111dc388e10a308eaba26b656
Riccardo Albiero <albr.riccardo@gmail.com>
2024-12-28 18:25:03 +0100

    Set cpp tabspace value

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,4 +1,10 @@
 -- c++
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.fileformat = 'unix'
+vim.opt.foldlevelstart = 99
 
 -- build and execute
 local opts = { noremap = true, silent = true }
