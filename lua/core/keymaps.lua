e2e32fe30560ce4c8405407ab5eb26be89aa8168
Riccardo Albiero <albr.riccardo@gmail.com>
2025-01-01 12:16:12 +0100

    Update keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,14 +9,14 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- keymaps for better default experience
-keymap('n', '\\', ':bd<CR>', opts)
-keymap('n', '<Tab>', ':bnext<CR>', opts)
-keymap('n', '<S-Tab>', ':bprevious<CR>', opts)
+keymap('n', '\\', '<cmd> bd<CR>', opts)
+keymap('n', '<Tab>', '<cmd> bnext<CR>', opts)
+keymap('n', '<S-Tab>', '<cmd> bprevious<CR>', opts)
 
 -- mapping C-s to save the file, in all the modes
-keymap('n', '<C-s>', '<Esc>:Update<CR>', opts)
-keymap('v', '<C-s>', '<Esc>:Update<CR>gv', opts)
-keymap('i', '<C-s>', '<Esc>:Update<CR>', opts)
+keymap('n', '<C-s>', '<Esc> <cmd>Update<CR>', opts)
+keymap('v', '<C-s>', '<Esc> <cmd>Update<CR>gv', opts)
+keymap('i', '<C-s>', '<Esc> <cmd>Update<CR>', opts)
 
 -- keep searches centered on screen
 keymap('n', 'n', 'nzz', opts)
@@ -27,7 +27,7 @@ keymap('n', 'g*', 'g*zz', opts)
 keymap('n', 'g#', 'g#zz', opts)
 
 -- join lines keepin the cursor position
-keymap('n', 'J', ':let p=getpos(".")<bar>join<bar>call setpos(".", p)<CR>', opts)
+keymap('n', 'J', '<cmd>let p=getpos(".")<bar>join<bar>call setpos(".", p)<CR>', opts)
 
 -- move vertically by visual line (don't skip wrapped lines)
 keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -38,14 +38,10 @@ keymap('v', '<', '<gv', opts)
 keymap('v', '>', '>gv', opts)
 
 -- argslist navigation
-keymap('n', '[a', ':previous<CR>', opts)
-keymap('n', ']a', ':next<CR>', opts)
-keymap('n', '[A', ':first<CR>', opts)
-keymap('n', ']A', ':last<CR>', opts)
-
--- quickfix list navigation
-keymap('n', '[q', ':cp<CR>', opts)
-keymap('n', ']q', ':cn<CR>', opts)
+keymap('n', '[a', '<cmd>previous<CR>', opts)
+keymap('n', ']a', '<cmd>next<CR>', opts)
+keymap('n', '[A', '<cmd>first<CR>', opts)
+keymap('n', ']A', '<cmd>last<CR>', opts)
 
 -- paste without overriding the register
 keymap('x', 'p', [["_dP]])
@@ -68,8 +64,8 @@ keymap('t', '<C-l>', '<C-\\><C-N><C-w>l', term_opts)
 
 -- window management
 keymap('n', '<Leader>0', '<C-w>=', opts)
-keymap('n', '<Leader>+', ':vertical resize +10<CR>', opts)
-keymap('n', '<Leader>-', ':vertical resize -10<CR>', opts)
+keymap('n', '<Leader>+', '<cmd> vertical resize +10<CR>', opts)
+keymap('n', '<Leader>-', '<cmd>  resize -10<CR>', opts)
 
 -- vim.cmd [[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definition()<CR>]]
 -- vim.cmd [[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
@@ -87,14 +83,16 @@ keymap('i', '<Left>', '<NOP>', opts)
 keymap('i', '<Right>', '<NOP>', opts)
 
 -- diagnostic keymaps
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+keymap('n', '<M-j>', '<cmd> cnext<CR>', { desc = 'Go to next quickfix list argument' })
+keymap('n', '<M-k>', '<cmd> cprev<CR>', { desc = 'Go to previous quickfix list argument' })
 
 -- change directory to cwd
-keymap('n', '<leader>cd', ':cd %:h<CR>', opts)
+keymap('n', '<leader>cd', '<cmd> cd %:h<CR>', opts)
 
 -- notes
-keymap('n', '<leader>ni', ':e $NOTES_DIR/index.md<CR>:cd $NOTES_DIR<CR>', opts)
-keymap('n', '<leader>nt', ':silent !ctags -R . <CR> :redraw! <CR>', opts)
+keymap('n', '<leader>ni', '<cmd> e $NOTES_DIR/index.md<CR>:cd $NOTES_DIR<CR>', opts)
+keymap('n', '<leader>nt', '<cmd> silent !ctags -R . <CR> :redraw! <CR>', opts)
