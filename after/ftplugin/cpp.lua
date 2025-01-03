da8df64ae075d6a7a6db6dc6bc5d5020f383d352
Riccardo Albiero <albr.riccardo@gmail.com>
2025-01-03 17:58:41 +0100

    Set makeprg for cpp files

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -6,35 +6,23 @@ vim.opt.expandtab = true
 vim.opt.fileformat = 'unix'
 vim.opt.foldlevelstart = 99
 
--- build and execute
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<F7>', ':w <bar> !g++ -std=c++17 -Wall -m64 -g -I include % -o %:r <CR>', opts)
 
-vim.keymap.set('i', '<F7>', '<Esc>:w <bar> !g++ -std=c++17 -Wall -m64 -g -I include % -o %:r <CR>', opts)
-vim.keymap.set('i', '<F8>', '<Esc>:!./%:r<CR>', opts)
+-- make program (compile with sanitizers)
+vim.opt.makeprg = 'g++ -std=c++17 -Wshadow -Wall -g -I include % -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -o %:r'
+vim.keymap.set('n', '<F6>', '<cmd> make<CR>', opts)
+vim.keymap.set('i', '<F6>', '<Esc> <cmd>make <CR>', opts)
 
 -- compile with -O2 flag (optimized)
 vim.keymap.set('n', '<F7>', ':w <bar> !g++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -I include % -o %:r <CR>', opts)
 vim.keymap.set('i', '<F7>', '<Esc> :w <bar> !g++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -I include % -o %:r <CR>', opts)
--- compile with sanitizers
-vim.keymap.set(
-  'n',
-  '<F9>',
-  ':w <bar> !g++ -std=c++17 -Wshadow -Wall -o %:r -g -I include % -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG <CR>',
-  opts
-)
-vim.keymap.set(
-  'i',
-  '<F9>',
-  '<Esc> :w <bar> !g++ -std=c++17 -Wshadow -Wall -o %:r -g -I include % -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG <CR>',
-  opts
-)
 
 -- execute
-vim.keymap.set('i', '<F8>', '<Esc>:!./%:r', opts)
+vim.keymap.set('n', '<F8>', '<cmd> !./%:r <CR>', opts)
+vim.keymap.set('i', '<F8>', '<Esc> <cmd>!./%:r <CR>', opts)
 
 -- debugging
-vim.keymap.set('n', '<F9>', ':!gdb ./%:r<CR>', opts)
+vim.keymap.set('n', '<F9>', '<cmd> gdb ./%:r<CR>', opts)
 
 -- NB: to remove/change these mappings use :mapclear <buffer>
 --     see also :h map-<buffer>
