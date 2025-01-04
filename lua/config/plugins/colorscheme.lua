02aef2e2e2df6fcff32f5958b7cf4f982f437dcc
Riccardo Albiero <albr.riccardo@gmail.com>
2025-01-04 19:58:09 +0100

    Fix colorscheme

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,24 +1,25 @@
-vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GitSignsDelete' })
-vim.api.nvim_create_augroup('UpdateGutter', { clear = true })
-vim.api.nvim_create_autocmd('ColorScheme', {
-  group = 'UpdateGutter',
-  pattern = '*',
-  callback = function()
-    vim.cmd [[highlight ColorColumn guibg=#000000]]
-    vim.cmd [[highlight GitSignsAdd guifg=#006900]]
-    vim.cmd [[highlight GitSignsChange guifg=#FFFF00]]
-    vim.cmd [[highlight GitSignsDelete guifg=#FF0000]]
-    vim.cmd [[highlight GitSignsChangedelete guifg=#FF0000]]
-    vim.cmd [[highlight GitSignsTopdelete guifg=#FF0000]]
-    vim.cmd [[highlight MatchParen ctermbg=240 ctermfg=186 cterm=NONE guibg=#585858 guifg=#DFDF87 gui=NONE]]
-  end,
-})
-vim.cmd 'colorscheme lunaperche'
-
 return {
-  'ntk148v/komau.vim',
-  'Shatur/neovim-ayu',
-  'eldritch-theme/eldritch.nvim',
-  'gremble0/yellowbeans.nvim',
-  { 'catppuccin/nvim', name = 'catppuccin' },
+  "gremble0/yellowbeans.nvim",
+  priority = 1000, -- Theme should load before most other plugins
+  config = function()
+        vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GitSignsDelete' })
+        vim.api.nvim_create_augroup('UpdateGutter', { clear = true })
+        vim.api.nvim_create_autocmd('ColorScheme', {
+          group = 'UpdateGutter',
+          pattern = '*',
+          callback = function()
+            vim.cmd ("highlight ColorColumn guibg=#000000")
+            vim.cmd ("highlight GitSignsAdd guifg=#006900")
+            vim.cmd ("highlight GitSignsChange guifg=#FFFF00")
+            vim.cmd ("highlight GitSignsDelete guifg=#FF0000")
+            vim.cmd ("highlight GitSignsChangedelete guifg=#FF0000")
+            vim.cmd ("highlight GitSignsTopdelete guifg=#FF0000")
+        vim.cmd ("highlight GitSignsTopdelete guifg=#FF0000")
+            vim.cmd ("highlight MatchParen ctermbg=240 ctermfg=186 cterm=NONE guibg=#585858 guifg=#DFDF87 gui=NONE")
+            vim.cmd ("highlight Normal ctermbg=black guibg=#000000")
+            vim.cmd("highlight SignColumn ctermbg=black guibg=#000000")
+          end,
+        })
+        vim.cmd.colorscheme("yellowbeans")
+  end,
 }
