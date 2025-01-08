41f13beab0ce07bfe2226d010fd35f7b8f34818d
Riccardo Albiero <albr.riccardo@gmail.com>
2025-01-08 13:36:03 +0100

    Make the lualine theme transparent

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -1,9 +1,24 @@
+local custom_theme = require 'lualine.themes.yellowbeans-monochrome'
+
+custom_theme.normal.a.bg = '#000000'
+custom_theme.normal.b.bg = '#000000'
+custom_theme.normal.c.bg = '#000000'
+
+custom_theme.insert.a.bg = '#000000'
+custom_theme.command.a.bg = '#000000'
+custom_theme.replace.a.bg = '#000000'
+custom_theme.visual.a.bg = '#000000'
+
+custom_theme.inactive.a.bg = '#000000'
+custom_theme.inactive.b.bg = '#000000'
+custom_theme.inactive.c.bg = '#000000'
+
 return {
   'nvim-lualine/lualine.nvim',
   event = 'BufEnter',
   opts = {
     options = {
-      theme = 'iceberg_dark',
+      theme = custom_theme,
       section_separators = { left = '', right = '' },
       component_separators = { left = '', right = '' },
       globalstatus = true,
