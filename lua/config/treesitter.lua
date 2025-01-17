0097834873f845790f3cddab0e5b6fa8d350017f
ricalbr <albr.riccardo@gmail.com>
2025-01-17 12:08:26 +0100

    Add required parsers from :checkhealth

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,3 +1,3 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'vimdoc', 'latex', 'vim', 'bash' },
+  ensure_installed = { 'bash', 'c', 'cpp', 'latex', 'lua', 'markdown', 'markdown_inline', 'python', 'query', 'vim', 'vimdoc' },
 }
