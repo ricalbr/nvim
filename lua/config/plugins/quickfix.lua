d6a98ab4838a9b7cd95d1db1257319b721e27f13
ricalbr <albr.riccardo@gmail.com>
2025-01-18 22:58:44 +0100

    Remove unused plugin

--- lua/config/plugins/quickfix.lua
+++ lua/config/plugins/quickfix.lua
@@ -1,12 +1,5 @@
 return {
   {
-    'kevinhwang91/nvim-bqf',
-    event = 'BufEnter',
-    config = function()
-      require('bqf').setup()
-    end,
-  },
-  {
     'stevearc/quicker.nvim',
     enabled = false,
     event = 'FileType qf',
