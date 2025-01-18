98029a461aac5b169ab7f137c778c91753e17239
ricalbr <albr.riccardo@gmail.com>
2025-01-18 14:42:04 +0100

    Make keymaps for running code consistent

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -5,7 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.fileformat = "unix"
+vim.opt.fileformat = 'unix'
 vim.opt.textwidth = 120
 vim.wo.colorcolumn = 121
 -- vim.opt.foldlevel = 0
@@ -13,5 +13,5 @@ vim.opt.foldlevelstart = 99
 
 -- mappings
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<F5>", ':update <CR>:exec "!python3" shellescape(@%, 1)<CR>', opts)
-vim.keymap.set("i", "<F5>", '<ESC> :update <CR>:exec "!python3" shellescape(@%, 1)<CR>', opts)
+vim.keymap.set('n', '<F7>', ':update <CR>:exec "!python3" shellescape(@%, 1)<CR>', opts)
+vim.keymap.set('i', '<F7>', '<ESC> :update <CR>:exec "!python3" shellescape(@%, 1)<CR>', opts)
