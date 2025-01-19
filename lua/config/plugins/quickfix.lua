eec7dca7ec6e515819cabad0f1c41317f7f3f2b6
ricalbr <albr.riccardo@gmail.com>
2025-01-19 13:23:17 +0100

    Associate qf plugin to qf filetype

--- lua/config/plugins/quickfix.lua
+++ lua/config/plugins/quickfix.lua
@@ -1,8 +1,9 @@
 return {
   {
     'stevearc/quicker.nvim',
-    enabled = false,
-    event = 'FileType qf',
+    -- enabled = false,
+    -- event = 'FileType qf',
+    ft = 'qf',
     ---@module "quicker"
     ---@type quicker.SetupOptions
     opts = {},
