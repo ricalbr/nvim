5b5ef5fe8c1d9bc0b91b6941a625747ab5c745ae
ricalbr <albr.riccardo@gmail.com>
2025-01-20 17:28:38 +0100

    Reformat file

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -15,9 +15,7 @@ return {
         },
         delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
-        view_options = {
-          show_hidden = true,
-        },
+        view_options = { show_hidden = true },
       }
     end,
   },
