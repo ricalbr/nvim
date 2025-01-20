698f5a1196dfee2c0b5b86388e5e91b38558a4b2
ricalbr <albr.riccardo@gmail.com>
2025-01-20 13:57:09 +0100

    Set mini.surround to tpope's vim-surround behaviour

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,17 +7,31 @@ return {
       require('mini.bracketed').setup()
       require('mini.comment').setup()
       require('mini.jump').setup()
-      -- require('mini.pairs').setup()
+      require('mini.pairs').setup()
       require('mini.tabline').setup { show_icons = false }
-      -- require('mini.surround').setup()
-    end,
-  },
+      require('mini.surround').setup {
+        mappings = {
+          add = 'ys', --  defult  'sa'
+          delete = 'ds', --  defult  'sd'
+          find = '', --  defult  'sf'
+          find_left = '', --  defult  'sF'
+          highlight = '', --  defult  'sh'
+          replace = 'cs', --  defult  'sr'
+          update_n_lines = '', --  defult  'sn'
 
-  {
-    'kylechui/nvim-surround',
-    version = '*',
-    event = 'BufReadPre',
-    opts = {},
+          suffix_last = '',
+          suffix_next = '',
+        },
+        search_method = 'cover_or_next',
+      }
+
+      -- Remap adding surrounding to Visual mode selection
+      vim.keymap.del('x', 'ys')
+      vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+      -- Make special mapping for "add surrounding for line"
+      vim.keymap.set('n', 'yss', 'ys_', { remap = true })
+    end,
   },
 
   {
