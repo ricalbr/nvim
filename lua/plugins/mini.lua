72f83f39ea9703c4fd67c4c1c996064ef35b56d8
ricalbr <albr.riccardo@gmail.com>
2025-01-20 10:31:46 +0100

    Use other minor plugin instead of mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,13 +4,26 @@ return {
     version = '*',
     event = 'BufReadPre',
     config = function()
-      require('mini.ai').setup()
       require('mini.bracketed').setup()
       require('mini.comment').setup()
       require('mini.jump').setup()
-      require('mini.pairs').setup()
+      -- require('mini.pairs').setup()
       require('mini.tabline').setup { show_icons = false }
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
     end,
+  },
+
+  {
+    'kylechui/nvim-surround',
+    version = '*',
+    event = 'BufReadPre',
+    opts = {},
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    opts = {},
   },
 }
