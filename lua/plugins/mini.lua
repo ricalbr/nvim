ea03e07c3cb8c9318db91859154a07b52aaa7fa4
ricalbr <albr.riccardo@gmail.com>
2025-01-31 11:10:01 +0100

    Improve formatting and mini settings

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,3 @@
-local icons = require 'plugins.config.icons'
 return {
   {
     'echasnovski/mini.nvim',
@@ -10,18 +9,14 @@ return {
       require('mini.comment').setup()
       require('mini.icons').setup()
       require('mini.jump').setup()
-      -- require('mini.pairs').setup()
+      require('mini.pairs').setup()
       require('mini.tabline').setup { show_icons = false }
       -- require('mini.statusline').setup()
       -- require('mini.pick').setup()
       require('mini.diff').setup {
         view = {
           style = 'sign',
-          signs = {
-            add = icons.ui.BoldLineMiddle,
-            change = icons.ui.BoldLineMiddle,
-            delete = icons.ui.TriangleShortArrowRight,
-          },
+          signs = { add = '▌', change = '▌', delete = '▌' },
         },
         mappings = {
           apply = 'ha',
@@ -29,37 +24,37 @@ return {
         },
       }
       require('mini.surround').setup {
+        --stylua: ignore
         mappings = {
-          add = 'ys', --  defult  'sa'
-          delete = 'ds', --  defult  'sd'
-          find = '', --  defult  'sf'
-          find_left = '', --  defult  'sF'
-          highlight = '', --  defult  'sh'
-          replace = 'cs', --  defult  'sr'
-          update_n_lines = '', --  defult  'sn'
-
-          suffix_last = '',
-          suffix_next = '',
+            add             =  'ys',  --  defult  'sa'
+            delete          =  'ds',  --  defult  'sd'
+            find            =  '',    --  defult  'sf'
+            find_left       =  '',    --  defult  'sF'
+            highlight       =  '',    --  defult  'sh'
+            replace         =  'cs',  --  defult  'sr'
+            update_n_lines  =  '',    --  defult  'sn'
+            suffix_last     =  '',
+            suffix_next     =  '',
         },
         search_method = 'cover_or_next',
       }
 
-      -- Remap adding surrounding to Visual mode selection
+      -- remap adding surrounding to visual mode selection
       vim.keymap.del('x', 'ys')
       vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
 
-      -- Make special mapping for "add surrounding for line"
+      -- make special mapping for "add surrounding for line"
       vim.keymap.set('n', 'yss', 'ys_', { remap = true })
       MiniIcons.mock_nvim_web_devicons()
     end,
   },
 
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    opts = {},
-  },
+  -- {
+  --   'windwp/nvim-autopairs',
+  --   event = 'InsertEnter',
+  --   config = true,
+  --   opts = {},
+  -- },
 
   {
     'takac/vim-hardtime',
