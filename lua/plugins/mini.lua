f1862ce2444ac5c617520a77a31daf023398d009
ricalbr <albr.riccardo@gmail.com>
2025-02-02 00:21:37 +0100

    Format code with shiftwidth = 4

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,50 +4,50 @@ return {
         version = false,
         event = 'BufReadPre',
 
-    --stylua: ignore
-    config = function()
-      require('mini.ai').setup()
-      require('mini.bracketed').setup()
-      require('mini.comment').setup()
-      require('mini.indentscope').setup { draw = { animation = function() return 0 end, } }
-      require('mini.icons').setup{lsp={['snippet'] = { glyph = '', hl = 'MiniIconsGreen' }}}
-      require('mini.jump').setup()
-      require('mini.pairs').setup()
-      require('mini.tabline').setup { show_icons = false }
-      require('mini.trailspace').setup()
-      -- require('mini.statusline').setup()
-      -- require('mini.pick').setup()
-      require('mini.diff').setup {
-        view = {
-          style = 'sign',
-          signs = { add = '▌', change = '▌', delete = '▌' },
-        },
-        mappings = { apply = 'ha', reset = 'hr' },
-      }
-      require('mini.surround').setup {
-        mappings = {
-            add             =  'ys',  --  defult  'sa'
-            delete          =  'ds',  --  defult  'sd'
-            find            =  '',    --  defult  'sf'
-            find_left       =  '',    --  defult  'sF'
-            highlight       =  '',    --  defult  'sh'
-            replace         =  'cs',  --  defult  'sr'
-            update_n_lines  =  '',    --  defult  'sn'
-            suffix_last     =  '',
-            suffix_next     =  '',
-        },
-        search_method = 'cover_or_next',
-      }
+        --stylua: ignore
+        config = function()
+            require('mini.ai').setup()
+            require('mini.bracketed').setup()
+            require('mini.comment').setup()
+            require('mini.indentscope').setup { draw = { animation = function() return 0 end } }
+            require('mini.icons').setup { lsp = { ['snippet'] = { glyph = '' } } }
+            require('mini.jump').setup()
+            require('mini.pairs').setup()
+            require('mini.tabline').setup { show_icons = false }
+            require('mini.trailspace').setup()
+            -- require('mini.statusline').setup()
+            -- require('mini.pick').setup()
+            require('mini.diff').setup {
+                view = {
+                    style = 'sign',
+                    signs = { add = '▌', change = '▌', delete = '▌' },
+                },
+                mappings = { apply = 'ha', reset = 'hr' },
+            }
+            require('mini.surround').setup {
+                mappings = {
+                    add             =  'ys', --  defult  'sa'
+                    delete          =  'ds', --  defult  'sd'
+                    find            =  '', --  defult  'sf'
+                    find_left       =  '', --  defult  'sF'
+                    highlight       =  '', --  defult  'sh'
+                    replace         =  'cs', --  defult  'sr'
+                    update_n_lines  =  '', --  defult  'sn'
+                    suffix_last     =  '',
+                    suffix_next     =  '',
+                },
+                search_method = 'cover_or_next',
+            }
 
-      -- remap adding surrounding to visual mode selection
-      vim.keymap.del('x', 'ys')
-      vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+            -- remap adding surrounding to visual mode selection
+            vim.keymap.del('x', 'ys')
+            vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
 
-      -- make special mapping for "add surrounding for line"
-      vim.keymap.set('n', 'yss', 'ys_', { remap = true })
+            -- make special mapping for "add surrounding for line"
+            vim.keymap.set('n', 'yss', 'ys_', { remap = true })
 
-      MiniIcons.mock_nvim_web_devicons()
-    end,
+            MiniIcons.mock_nvim_web_devicons()
+        end,
     },
 
     {
