3589ab36ee36ce5d6f6b99dba26c184e413e62c5
ricalbr <albr.riccardo@gmail.com>
2025-02-10 14:19:22 +0100

    Remove preview borders

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -10,20 +10,19 @@ return {
                     syntax_limit_b = -102400, -- 100KB limit on highlighting files
                 },
             },
-
             winopts = {
                 preview = {
                     default = 'bat',
                     title = false,
-                    wrap = true,
+                    border = 'none',
                 },
+                treesitter = false,
             },
             grep = {
                 rg_glob = true,
                 rg_opts = '--color=never --no-heading --with-filename --line-number --column --smart-case --hidden --glob=!.git/',
             },
             defaults = {
-                winopts = { treesitter = false },
                 git_icons = false,
                 file_icons = false,
                 color_icons = false,
