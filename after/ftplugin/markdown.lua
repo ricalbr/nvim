e9c8be6e6783a967987d7a6bb52cc1e56af2afe3
ricalbr <albr.riccardo@gmail.com>
2025-02-10 17:16:09 +0100

    Add spelling settings also for Markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,9 +4,13 @@
 vim.opt.linebreak = true
 vim.opt.formatoptions = vim.opt.formatoptions - 't'
 
+-- spelling
+local spell = require 'core.spell'
+spell()
+
 -- keymaps
 local opts = { noremap = true, silent = true }
 vim.keymap.set('i', '<C-l>', '<c-g>u<Esc>[s1z=`]a<c-g>u', opts)
 
--- commment string
+-- comment string
 vim.opt.commentstring = '<!-- %s -->'
