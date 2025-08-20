6afd76f2755d14dde199edce3df1b48f33759dc9
Riccardo Albiero <albr.riccardo@gmail.com>
2025-08-20 16:52:26 +0200

    add custom colorscheme

--- init.lua
+++ init.lua
@@ -6,43 +6,45 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- general
-vim.opt.clipboard = 'unnamed'
-vim.opt.undofile = true
-vim.opt.mouse = 'a'
-vim.opt.winborder = 'rounded'
-vim.opt.cursorline = true
-vim.opt.ignorecase = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.smartindent = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn = 'yes'
-vim.opt.completeopt = 'menu,menuone,popup,fuzzy,noinsert'
+vim.o.clipboard = 'unnamed'
+vim.o.undofile = true
+vim.o.mouse = 'a'
+vim.o.winborder = 'rounded'
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.smartindent = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.termguicolors = true
+vim.o.signcolumn = 'yes'
+vim.o.completeopt = 'menu,menuone,popup,fuzzy,noinsert'
 
 -- plugins
 vim.pack.add({
     { src = 'https://github.com/stevearc/oil.nvim' },
     { src = 'https://github.com/echasnovski/mini.nvim' },
     { src = 'https://github.com/ibhagwan/fzf-lua' },
-    -- { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version="master"},
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/mason-org/mason.nvim' },
+
 })
 
 require 'mason'.setup()
 require 'fzf-lua'.setup()
 require 'plugins.mini'
 require 'plugins.oil'
+vim.cmd('colorscheme spring')
 
 -- keymaps and autocmds
 require 'keymaps'
 require 'autocmd'
 
-vim.lsp.enable({ 'lua_ls', 'clangd', 'codespell', 'prettier', 'pyright', 'ltex-ls-plus' })
--- require('nvim-treesitter.configs').setup({
---     ensure_installed = { 'lua', 'python', 'c', 'cpp' },
---     highlight = { enable = true },
---     indent = { enable = true },
--- })
+vim.lsp.enable({ 'lua_ls', 'clangd', 'codespell', 'prettier', 'pyright', 'tinymist' })
+require('nvim-treesitter.configs').setup({
+    ensure_installed = { 'lua', 'python', 'c', 'cpp' , 'typst'},
+    highlight = { enable = true },
+    indent = { enable = true },
+})
