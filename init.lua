cf6397aa9d42f14e520579b98902c139998b1b76
ricalbr <albr.riccardo@gmail.com>
2025-08-22 10:02:50 +0200

    (chore): remove cspell lsp

--- init.lua
+++ init.lua
@@ -29,7 +29,6 @@ vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/mason-org/mason.nvim' },
-
 })
 
 require 'mason'.setup()
@@ -42,7 +41,7 @@ vim.cmd('colorscheme spring')
 require 'keymaps'
 require 'autocmd'
 
-vim.lsp.enable({ 'lua_ls', 'clangd', 'cspell_ls', 'pyright', 'tinymist' })
+vim.lsp.enable({ 'lua_ls', 'clangd', 'pyright', 'tinymist' })
 require('nvim-treesitter.configs').setup({
     ensure_installed = { 'lua', 'python', 'c', 'cpp', 'typst' },
     highlight = { enable = true },
