5ab5d3c5390d533f56ca91eae120af96a918fcb6
ricalbr <albr.riccardo@gmail.com>
2025-09-04 15:41:29 +0200

    (feat) add lsp autotrigger and autoformat

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -86,19 +86,42 @@ autocmd('FileType', {
 })
 
 
--- autoformat on save
-autocmd("BufWritePre", {
-    group = augroup 'LspFormatting',
-    callback = function()
-        local mode = vim.api.nvim_get_mode().mode
-        local filetype = vim.bo.filetype
-        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
-            vim.cmd('lua vim.lsp.buf.format()')
-        else
-        end
-    end
-})
+-- -- autoformat on save
+-- autocmd("BufWritePre", {
+--     group = augroup 'LspFormatting',
+--     callback = function()
+--         local mode = vim.api.nvim_get_mode().mode
+--         local filetype = vim.bo.filetype
+--         if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+--             vim.cmd('lua vim.lsp.buf.format()')
+--         else
+--         end
+--     end
+-- })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+        if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+        end
+
+        -- Auto-format ("lint") on save.
+        if not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                end,
+            })
+        end
+    end,
+})
 -- overload q and w command
 vim.api.nvim_command 'command! Q q'
 vim.api.nvim_command 'command! W w'
