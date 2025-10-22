1e3926202d5ef52a90eb3f36c1845f88100b8b8f
ricalbr <albr.riccardo@gmail.com>
2025-10-22 11:42:43 +0200

    (feat) add autocompletion plugin

--- init.lua
+++ init.lua
@@ -30,6 +30,8 @@ vim.pack.add({
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/mason-org/mason.nvim' },
+    { src = "https://github.com/Saghen/blink.cmp",                version = "v1.6.0" },
+    { src = 'https://github.com/rafamadriz/friendly-snippets' }
 })
 
 require 'mason'.setup()
@@ -42,7 +44,22 @@ vim.cmd('colorscheme spring')
 require 'keymaps'
 require 'autocmd'
 
+-- blink
 vim.lsp.enable({ 'lua_ls', 'clangd', 'ruff', 'tinymist' })
+require("blink.cmp").setup({
+    {
+        keymap = { preset = 'default' },
+        appearance = { nerd_font_variant = 'mono' },
+        completion = { documentation = { auto_show = false } },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+        fuzzy = { implementation = "rust_prefered" }
+    },
+    opts_extend = { "sources.default" }
+})
+
+-- treesitter
 require('nvim-treesitter.configs').setup({
     ensure_installed = { 'lua', 'python', 'c', 'cpp', 'typst' },
     highlight = { enable = true },
--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -98,12 +98,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-        end
-
-        -- Auto-format ("lint") on save.
+        -- auto-format ("lint") on save.
         if not client:supports_method('textDocument/willSaveWaitUntil')
             and client:supports_method('textDocument/formatting') then
             vim.api.nvim_create_autocmd('BufWritePre', {
@@ -116,6 +111,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end
     end,
 })
+
 -- overload q and w command
 vim.api.nvim_command 'command! Q q'
 vim.api.nvim_command 'command! W w'
