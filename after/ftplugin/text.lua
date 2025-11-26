11be1bbab61c7b69a6fca29f521088f31d6e1c04
ricalbr <albr.riccardo@gmail.com>
2025-11-26 17:38:31 +0100

    (chore) add txt configurations

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -6,23 +6,10 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.linebreak = true
 
--- -- spelling
--- local spell = require 'core.spell'
--- spell()
+vim.opt.spell = true
+vim.opt.spelllang = { "en", "it" }
 
--- twitter mode
-vim.keymap.set('n', '<leader>tw', ':match Error "%>281v.+"<CR>')
-
--- let s:activatedt = 0
--- function! TwitterMode()
---     if s:activatedt == 0
---         let s:activatedt = 1
---         match Error '\%>281v.\+'
---     else
---         let s:activatedt = 0
---         match none
---     endif
--- endfunction
-
--- nnoremap <leader>tw :call TwitterMode()<CR>
