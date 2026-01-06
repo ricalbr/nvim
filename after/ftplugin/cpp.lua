f783089a143126d97202f30e4e01dd5cd01c15c4
ricalbr <albr.riccardo@gmail.com>
2026-01-06 22:04:47 +0100

    (chore) fix folding for cpp files

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -4,19 +4,23 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.fileformat = 'unix'
+vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.commentstring = '//%s' -- avoid /*%s*/ commstring
 
 local opts = { noremap = true, silent = true }
 
 -- make program (compile with sanitizers)
-vim.opt.makeprg = 'g++ -std=c++17 -Wshadow -Wall -g -I include % -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -o %:r'
+vim.opt.makeprg =
+'g++ -std=c++17 -Wshadow -Wall -g -I include % -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -o %:r'
 vim.keymap.set('n', '<F6>', '<cmd> make<CR>', opts)
 vim.keymap.set('i', '<F6>', '<Esc> <cmd>make <CR>', opts)
 
 -- compile with -O2 flag (optimized)
-vim.keymap.set('n', '<F7>', '<cmd>w <bar> !g++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -I include % -o %:r <CR>', opts)
-vim.keymap.set('i', '<F7>', '<Esc><cmd>w <bar> !g++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -I include % -o %:r <CR>', opts)
+vim.keymap.set('n', '<F7>', '<cmd>w <bar> !g++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -I include % -o %:r <CR>',
+    opts)
+vim.keymap.set('i', '<F7>',
+    '<Esc><cmd>w <bar> !g++ -std=c++17 -Wshadow -Wall -O2 -Wno-unused-result -I include % -o %:r <CR>', opts)
 
 -- execute
 vim.keymap.set('n', '<F8>', '<cmd> !./%:r <CR>', opts)
