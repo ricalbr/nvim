b51593afbcaa18b2354db323b1eeed871b8fa620
ricalbr <albr.riccardo@gmail.com>
2026-01-06 22:05:04 +0100

    (feat) add vim-cool autocmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -93,6 +93,16 @@ autocmd("BufWritePre", {
     end,
 })
 
+-- nohighlight, vim-cool substitute
+vim.api.nvim_create_autocmd('CursorMoved', {
+    group = vim.api.nvim_create_augroup('auto-hlsearch', { clear = true }),
+    callback = function()
+        if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+            vim.schedule(function() vim.cmd.nohlsearch() end)
+        end
+    end
+})
+
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('my.lsp', {}),
     callback = function(args)
